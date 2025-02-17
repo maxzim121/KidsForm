5271cd62b59563255c63ce876e29fba1b688c166
maxzim121 <zimensm12@gmail.com>
2025-02-17 18:32:30 +0300

    Initial Commit

---
+++ KidsForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsForm
+//
+//  Created by Maksim Zimens on 17.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
