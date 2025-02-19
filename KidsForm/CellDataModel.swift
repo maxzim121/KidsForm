c7ef086df438d0a3ef4ddaa8a8e5960343d676ba
maxzim121 <zimensm12@gmail.com>
2025-02-19 12:05:56 +0300

    Добавил модель для данных ячейки

---
+++ KidsForm/CellDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CellDataModel.swift
+//  KidsForm
+//
+//  Created by Maksim Zimens on 19.02.2025.
+//
+
+import Foundation
+
+struct CellDataModel {
+    var name: String?
+    var age: String?
+}
